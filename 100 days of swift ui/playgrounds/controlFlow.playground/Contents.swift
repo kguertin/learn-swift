85b30dad4b8e98f009b31d877ae05dbcafdfa38e
Kevin Guertin <kevin.m.guertin@gmail.com>
2023-08-24 12:44:58 -0400

    for and while loops in swift

--- 100 days of swift ui/playgrounds/controlFlow.playground/Contents.swift
+++ 100 days of swift ui/playgrounds/controlFlow.playground/Contents.swift
@@ -121,9 +121,44 @@ for i in 1...12 {
     print("5 x \(i) is \(5 * i)")
 }
 
+//creates a range
 for i in 1...12 {
     print("The \(i) times table")
     for j in 1...12 {
         print("\(j) x \(i) is \(j * i)")
     }
+    print()
 }
+
+for i in 1...5 {
+    print("counting from 1 through 5: \(i)")
+}
+
+// Range excluding last number
+for i in 1..<5 {
+    print("counting from 1 up to 5: \(i)")
+}
+
+var lyric = "Haters gonna"
+
+for _ in 1...5 {
+    lyric += " hate"
+}
+print(lyric)
+
+var countdown = 10
+while countdown > 0 {
+print("\(countdown)...")
+    countdown -= 1
+}
+print("Blast off!")
+
+let id = Int.random(in: 1...1000)
+let ammount = Double.random(in: 0...1)
+
+var roll = 0
+while roll != 20 {
+    roll = Int.random(in: 1...20)
+    print("I rolled a \(roll)")
+}
+print("Critical Hit!")
