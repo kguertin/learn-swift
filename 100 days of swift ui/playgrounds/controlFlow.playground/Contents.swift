2595e1a617190dd84a869582de0d9a75cd60b7b7
Kevin Guertin <kevin.m.guertin@gmail.com>
2023-08-28 10:51:52 -0400

    loop break and continue

--- 100 days of swift ui/playgrounds/controlFlow.playground/Contents.swift
+++ 100 days of swift ui/playgrounds/controlFlow.playground/Contents.swift
@@ -162,3 +162,33 @@ while roll != 20 {
     print("I rolled a \(roll)")
 }
 print("Critical Hit!")
+
+let filenames = ["me.jpg", "work.txt", "sophie.jpg"]
+
+// Continue skips to next iteration of loop
+for filename in filenames {
+    if filename.hasSuffix(".jpg") == false {
+        continue
+    }
+    print("Found picture: \(filename)")
+}
+
+// Break skips all future interations
+let number1 = 4
+let number2 = 14
+var multiples = [Int]()
+
+for i in 1...100_000 {
+
+    if i.isMultiple(of: number1) && i.isMultiple(of: number2) {
+        multiples.append(i)
+        
+        if multiples.count == 10 {
+            break
+        }
+    }
+}
+
+print(multiples)
+
+
