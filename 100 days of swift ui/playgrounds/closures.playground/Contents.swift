a5def525e6aa329ab738cf017b34224068dd2d54
Kevin Guertin <kevin.m.guertin@gmail.com>
2023-09-12 17:35:23 -0400

    work on closures in swift

--- 100 days of swift ui/playgrounds/closures.playground/Contents.swift
+++ 100 days of swift ui/playgrounds/closures.playground/Contents.swift
@@ -1,3 +1,54 @@
 import UIKit
 
-var greeting = "Hello, playground"
+func greetUser() {
+    print("Hi there!")
+}
+
+greetUser()
+var greetCopy: () -> Void = greetUser
+greetCopy()
+
+// closure expression
+let sayHello = { (name: String) -> String in
+    "Hi \(name)!"
+}
+
+print(sayHello("Kevin"))
+
+func getUserData(for id: Int) -> String {
+    if id == 1989 {
+        return "Taylor Swift"
+    }
+    return "Anonymous"
+}
+
+let userData: (Int) -> String = getUserData
+print(userData(1989))
+
+let team = ["Gloria", "Suzanne", "Piper", "Tiffany", "Tasha"]
+let sortedTeam = team.sorted()
+print(sortedTeam)
+
+func captainFirstSorted(name1: String, name2: String ) -> Bool {
+    if name1 == "Suzanne" {
+        return true
+    } else if name2 == "Suzanne" {
+        return false
+    }
+    
+    return name1 < name2
+}
+
+print(team.sorted(by: captainFirstSorted))
+
+let captainFirstTeam = team.sorted(by: {(name1: String, name2: String) -> Bool in
+    if name1 == "Suzanne" {
+        return true
+    } else if name2 == "Suzanne" {
+        return false
+    }
+    
+    return name1 < name2
+})
+
+print(captainFirstTeam)
